221433bf3ee5096137712a84114f47a60b2d3268
0xmohsinpathan <0xmohsinpathan@gmail.com>
2023-08-13 00:08:25 +0530

    Added cursor and cursor shadow to the website. Updated script to animate the cursor and cursor shadow when the user moves the mouse. Updated style to style the cursor and cursor shadow.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-gsap.to("nav",{
+gsap.to("nav", {
     backgroundColor: "#000",
-    height:"104px",
-    duration:0.5,
-    scrollTrigger:{
-        trigger:"nav",
-        scroller:"body",
-        markers:true,
-        start:"top -8%",
+    height: "104px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "nav",
+        scroller: "body",
+        start: "top -8%",
         scrub: .8
+    }
+})
+var cursor = document.querySelector(".cursor")
+var cursorShadow = document.querySelector(".cursor-shadow")
+
+document.addEventListener("mousemove", function (mousePostion) {
+    cursorShadow.style.left = mousePostion.x - 200 + "px"
+    cursorShadow.style.top = mousePostion.y - 200 + "px"
+
+    cursor.style.left = mousePostion.x + "px"
+    cursor.style.top = mousePostion.y + "px"
+})
+
+gsap.to("main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "main",
+        scroller: "body",
+        markers: true,
+        start: "top -30%",
+        end: "top -80%",
+        scrub: 1
     }
 })
